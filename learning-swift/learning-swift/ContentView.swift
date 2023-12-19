eaacef8ff6e0914f0982e2eb32249468a888497d
mira-kine <mkine.echo@gmail.com>
2023-12-19 10:10:22 -0800

    practicing pull and push

---
+++ learning-swift/learning-swift/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  learning-swift
+//
+//  Created by MLT-024-SDE on 12/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Button("Hello") {
+            return print("Hello")
+        }
+        
+//        func greet(person: String) -> String {
+//            let greeting = "Hello," + person + "!"
+//            return greeting
+//        }
+//        
+//        print(greet(person: "Anna"))
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
